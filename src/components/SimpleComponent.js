e1f63bbaebdd137859fa774e0a91d2f995e8dba7
Logan Smith <logsmith06@gmail.com>
2021-04-20 22:38:33 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,9 +1,9 @@
 // Code SimpleComponent Here
 import React from 'react'
 const SimpleComponent = props => {
-render()
-return(){
+render(){
+return(
 
 <div onClick={props.handleClick}>I am just happy.</div>
-}
+)}
 }
